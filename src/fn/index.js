04e480a685f79141fd8320cbaabac7210724daa5
Anthony Crawford <anthonycrawford2@live.com>
2019-09-26 14:04:50 -0500

    develop: updated documents

--- src/fn/index.js
+++ src/fn/index.js
@@ -23,10 +23,50 @@ export {
     default as alt
 } from './alt';
 
-export {default as join} from './join';
-export {default as curry} from './curry';
-export {default as seq} from './seq';
-export {default as tap} from './tap';
+export {
+    /**
+     * This function will combines the results of functions funcA and funcB and return final result.
+     * @function
+     * @param {function} join - function manipulates the results of funcA & funcB and returns the results.
+     * @param {function} funcA - function takes data passed into and returns a result.
+     * @param {function} funcB - function takes data passed into and returns a result.
+     * @returns {any}
+     */
+    default as join
+} from './join';
+
+export {
+    /**
+     * This function takes a function and returns an a partial function of that function. 
+     * @function
+     * @param {function} curry - function we want to parital
+     * @param {any[]} [arguments] - optional array of arguments that will are passed to the curried function
+     * @returns {function}
+     */
+    default as curry
+} from './curry';
+
+export {
+    /**
+     * This function will loop through the series of functions passed in a arguments. Each function will be executed with the same data passed in as an argument.
+     * This function will return the original data that was passed into the series of funtions that were passed in as arguments.
+     * @function
+     * @param {...function} functions - function manipulates the results of funcA & funcB and returns the results.
+     * @returns {function}
+     */
+    default as seq
+} from './seq';
+
+export {
+    /**
+     * This function returns a function that will use either the passed in report function or console.log to log the current value passed tho the anonymous function it returns
+     * @function
+     * @param {function} report - function manipulates the results of funcA & funcB and returns the results.
+     * @returns {function}
+     */
+    default as tap
+} from './tap';
+
 export {default as identity} from './identity';
 export {default as wrap} from './wrap';
 export {default as always} from './always';
