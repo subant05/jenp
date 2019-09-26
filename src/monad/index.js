719611276dcf3d4ed51de55810a6cd4e84c1be02
Anthony Crawford <anthonycrawford2@live.com>
2019-09-26 18:08:48 -0500

    develop: updating docs

--- src/monad/index.js
+++ src/monad/index.js
@@ -1,11 +1,11 @@
 /**
- * @namespace monad
+ * @module monad
  */
 export {
     /**
      *  @description This monad Maybe and values of that type can either contain a value, or be an empty value.  A value of any type other then undefined or null, which is defined but used in the context of Maybe is Just. 
      * @class Maybe
-     * @memberof monad
+     * @memberof module:monad
      */
     default as Maybe
 } from './Maybe';
@@ -14,7 +14,7 @@ export {
     /**
      *  @description Either can be of 2 possibilities. The value is represented as either Left, which is error or Right which is an correct.Right is always favored
      * @class Either
-     * @memberof monad
+     * @memberof module:monad
      */
     default as Either
 } from './Either';
@@ -23,7 +23,7 @@ export {
     /**
      *  @description IO is monad that is simply an abstract data type. Actions are defined rather than invoked. IO is a good choice to handle DOM mutation and reducing side effects.
      * @class IO
-     * @memberof monad
+     * @memberof module:monad
      */
     default as IO
 } from './IO';
