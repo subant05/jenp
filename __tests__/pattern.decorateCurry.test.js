23e6fad192c67d3b0c95d244a4200c192cf924cd
subant05 <anthonycrawford2@mac.co>
2020-03-25 21:09:22 -0500

    refactored and added some more test

--- __tests__/pattern.decorateCurry.test.js
+++ __tests__/pattern.decorateCurry.test.js
@@ -8,8 +8,17 @@ test("Testing: pattern.decorateCurry()",()=>{
             this.breed = breed;
             this.type = type;
         }
+
+        getDescription(){
+            return `This ${this.color} dog is a ${this.breed} and it belongs to the ${this.type} group.`
+        }
     }
     const decoratedDog = Pattern.decorateCurry(Dog, {bark:()=>{return "Yap Yap"}})
     const littleDog = decoratedDog("tan","Chiuahua","Toy")
     expect(littleDog.bark()).toBe("Yap Yap")
+    expect(littleDog.breed).toBe("Chiuahua")
+    expect(littleDog.type).toBe("Toy")
+    expect(littleDog.color).toBe("tan")
+    expect(littleDog.getDescription()).toBe(`This tan dog is a Chiuahua and it belongs to the Toy group.`)
+
 })
--- src/pattern/decorateCurry.js
+++ src/pattern/decorateCurry.js
@@ -1,9 +1,9 @@
 export default function deccorateCurry(fn,decoration={}){
-    const proto =  Object.getPrototypeOf(fn)
 
     function Decorator(){
         fn.apply(this,arguments)
-    };
+    }
+
     Decorator.prototype = Object.assign(fn.prototype,decoration);
 
     return function(){
