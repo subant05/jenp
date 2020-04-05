8307e2155b33419d71ea2f8ad1b6d246767614e4
subant05 <anthonycrawford2@mac.co>
2020-04-04 22:37:15 -0500

    changed modules name from rx to reactive

--- src/stack.js
+++ src/stack.js
@@ -1,7 +1,7 @@
 import * as fn from './fn/index';
 import * as monad from './monad/index';
 import * as pattern from './pattern/index';
-import * as rx from './reactive/index';
+import * as reactive from './reactive/index';
 
 
 const stack = {
