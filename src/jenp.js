7050ed2366367901a040b26559c1b1b234312a4a
Anthony Crawford <anthonycrawford2@mac.com>
2023-03-07 22:21:48 -0600

    Added asycComposer

--- src/jenp.js
+++ src/jenp.js
@@ -1,18 +1,13 @@
-import * as fn from './fn/index';
-import * as monad from './monad/index';
-import * as pattern from './pattern/index';
-import * as reactive from './reactive/index';
+import * as fn from "./fn/index";
+import * as monad from "./monad/index";
+import * as pattern from "./pattern/index";
+import * as reactive from "./reactive/index";
 
 const jenp = {
-    fn
-    , monad
-    , pattern
-    , reactive
-}
+  fn,
+  monad,
+  pattern,
+  reactive,
+};
 
-if(window){
-    global.jenp = jenp;
-    global._j = jenp;
-} 
-
-export{ jenp as default}
+export { jenp };
